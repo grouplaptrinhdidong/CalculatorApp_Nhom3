ad9225c451220a75028785f4f27b4b8ef176a938
kietnt98 <kietanhthongminh@gmail.com>
2019-03-08 16:02:49 +0700

    số nguyên và số thực
    định dạng các kiểu số nguyên/số thực  và phép tính %

--- CalculatorApp/app/src/main/java/nhom3/calculatorapp/MainActivity.java
+++ CalculatorApp/app/src/main/java/nhom3/calculatorapp/MainActivity.java
@@ -3,13 +3,22 @@ package nhom3.calculatorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    //double value1 = 0;
-    //double value2 = 0;
-    public Double value1 = 0.0;
-    public Double value2 = 0.0;
+    // khai báo biến cờ để xác định số nguyên hay số thực
+    public int signDot = 0;
+    // khai báo biến cờ để xác định đã bấm dấu "=" chưa
+    public int signEqual = 0;
+    // khai báo số thứ 1
+    public String value1 = "0";
+    // khai báo số thứ 2
+    public String value2 = "0";
+    // khai báo biến lưu phép tính
     public Character sign = null;
 
     @Override
@@ -17,21 +26,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tvrs = (TextView) findViewById(R.id.tvResult);
-
+        final TextView tvResult = (TextView) findViewById(R.id.tvResult);
+        final EditText edtResult = (EditText) findViewById(R.id.edtInput);
 
         //tạo sự kiện click cho các button số từ 0 đến 9
         Button btnNum0 = (Button) findViewById(R.id.btnNum0);
         btnNum0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"0");
+                tvResult.setText(tvResult.getText().toString()+"0");
+                edtResult.setText(edtResult.getText().toString()+"0");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText().toString());
+                    value1 = tvResult.getText().toString();
                 }
                 else {
-                    //value2 = Double.parseDouble(tvrs.getText() + "");
-                    value2 = Double.parseDouble(tvrs.getText().toString());
+                    //value2 = Double.parseDouble(tvResult.getText() + "");
+                    value2 = tvResult.getText().toString();
                 }
             }
         });
@@ -39,14 +49,15 @@ public class MainActivity extends AppCompatActivity {
         btnNum1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"1");
+                tvResult.setText(tvResult.getText().toString()+"1");
+                edtResult.setText(edtResult.getText().toString()+"1");
                 if(sign == null){
-                    //value1 = Double.parseDouble(tvrs.getText() + "");
-                    value1 = Double.parseDouble(tvrs.getText().toString());
+                    //value1 = Double.parseDouble(tvResult.getText() + "");
+                    value1 = tvResult.getText().toString();
                 }
                 else {
-                    //value2 = Double.parseDouble(tvrs.getText() + "");
-                    value2 = Double.parseDouble(tvrs.getText().toString());
+                    //value2 = Double.parseDouble(tvResult.getText() + "");
+                    value2 = tvResult.getText().toString();
                 }
             }
         });
@@ -54,12 +65,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"2");
+                tvResult.setText(tvResult.getText().toString()+"2");
+                edtResult.setText(edtResult.getText().toString()+"2");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 = tvResult.getText() + "";
                 }
             }
         });
@@ -67,12 +79,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"3");
+                tvResult.setText(tvResult.getText().toString()+"3");
+                edtResult.setText(edtResult.getText().toString()+"3");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 = tvResult.getText() + "";
                 }
             }
         });
@@ -80,12 +93,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"4");
+                tvResult.setText(tvResult.getText().toString()+"4");
+                edtResult.setText(edtResult.getText().toString()+"4");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 =tvResult.getText() + "";
                 }
             }
         });
@@ -93,12 +107,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"5");
+                tvResult.setText(tvResult.getText().toString()+"5");
+                edtResult.setText(edtResult.getText().toString()+"5");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 = tvResult.getText() + "";
                 }
             }
         });
@@ -106,12 +121,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"6");
+                tvResult.setText(tvResult.getText().toString()+"6");
+                edtResult.setText(edtResult.getText().toString()+"6");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 = tvResult.getText() + "";
                 }
             }
         });
@@ -119,12 +135,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"7");
+                tvResult.setText(tvResult.getText().toString()+"7");
+                edtResult.setText(edtResult.getText().toString()+"7");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 =tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 =tvResult.getText() + "";
                 }
             }
         });
@@ -132,12 +149,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"8");
+                tvResult.setText(tvResult.getText().toString()+"8");
+                edtResult.setText(edtResult.getText().toString()+"8");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 =tvResult.getText() + "";
                 }
             }
         });
@@ -145,12 +163,13 @@ public class MainActivity extends AppCompatActivity {
         btnNum9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+"9");
+                tvResult.setText(tvResult.getText().toString()+"9");
+                edtResult.setText(edtResult.getText().toString()+"9");
                 if(sign == null){
-                    value1 = Double.parseDouble(tvrs.getText() + "");
+                    value1 = tvResult.getText() + "";
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText() + "");
+                    value2 =tvResult.getText() + "";
                 }
             }
         });
@@ -161,8 +180,17 @@ public class MainActivity extends AppCompatActivity {
         btnSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText("");
-                sign = '-';
+                if(signEqual!=0){
+                    tvResult.setText("");
+                    edtResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+ value1 +'-');
+                    sign = '-';
+                }
+                else {
+                    tvResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+"-");
+                    sign = '-';
+                }
             }
         });
 
@@ -171,8 +199,17 @@ public class MainActivity extends AppCompatActivity {
         btnDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText("");
-                sign = '/';
+                if(signEqual!=0){
+                    tvResult.setText("");
+                    edtResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+ value1 +'/');
+                    sign = '/';
+                }
+                else {
+                    tvResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+"/");
+                    sign = '/';
+                }
             }
         });
 
@@ -181,8 +218,17 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sign = '+';
-                tvrs.setText("");
+                if(signEqual!=0){
+                    tvResult.setText("");
+                    edtResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+ value1 +'+');
+                    sign = '+';
+                }
+                else {
+                    tvResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+"+");
+                    sign = '+';
+                }
             }
         });
 
@@ -191,8 +237,36 @@ public class MainActivity extends AppCompatActivity {
         btnMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sign ='*';
-                tvrs.setText("");
+                if(signEqual!=0){
+                    tvResult.setText("");
+                    edtResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+ value1 +'*');
+                    sign = '*';
+                }
+                else {
+                    tvResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+"*");
+                    sign = '*';
+                }
+            }
+        });
+
+        //Đặt dấu cho button %
+        Button btnPer=(Button) findViewById(R.id.btnPercent);
+        btnPer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(signEqual!=0){
+                    tvResult.setText("");
+                    edtResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+ value1 +'%');
+                    sign = '%';
+                }
+                else {
+                    tvResult.setText("");
+                    edtResult.setText(edtResult.getText().toString()+"%");
+                    sign = '%';
+                }
             }
         });
 
@@ -201,13 +275,15 @@ public class MainActivity extends AppCompatActivity {
         btnDot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvrs.setText(tvrs.getText().toString()+".");
+                tvResult.setText(tvResult.getText().toString()+".");
+                edtResult.setText(edtResult.getText().toString()+".");
                 if(sign==null){
-                    value1 = Double.parseDouble(tvrs.getText().toString());
+                    value1 = tvResult.getText().toString();
                 }
                 else {
-                    value2 = Double.parseDouble(tvrs.getText().toString());
+                    value2 = tvResult.getText().toString();
                 }
+                signDot=1;
             }
         });
 
@@ -217,35 +293,88 @@ public class MainActivity extends AppCompatActivity {
         btnEq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //double rs;
 
-                if(sign != null){
-                    Double rs = 0.0;
-                    //switch (sign)
-//                    {
-//                        case '+': rs = value1 + value2;break;
-//                        case '*': rs = value1 + value2;break;
-//                        case '-': rs = value1 - value2;break;
-//                        case '/': rs = value1/value2;break;
-//
-//                    }
-                    if(sign=='+'){
-                        rs= value1+value2;
-                    }
-                    else if(sign=='-'){
-                        rs=value1-value2;
-                    }
-                    else if (sign=='*'){
-                        rs=value1*value2;
-                    }
-                    else if (sign=='/'){
-                        rs=value1/value2;
-                    }
+                if(sign!=null){
+                    if(signDot!=0){
+                        double num1= Double.parseDouble(value1.toString()+"");
+                        double num2= Double.parseDouble(value2.toString()+"");
+                        double rs = 0;
+                        if(sign=='+'){
+                            rs = num1+num2;
+                        }
+                        else if(sign=='-'){
+                            rs = num1-num2 ;
+                        }
+                        else if (sign=='*'){
+                            rs = num1*num2;
+                        }
+                        else if (sign=='/'){
+                            rs = num1/num2;
+                        }
+                        else {
+                            if(sign=='%'){
+                                rs=num1/100;
+                            }
+                        }
 
-                    tvrs.setText(rs +"");
-                    tvrs.setText(rs.toString());
-                    value1 = rs;
-                    sign = null;
+                        tvResult.setText(rs +"");
+                        DecimalFormat df = new DecimalFormat("###.########");
+                        tvResult.setText(df.format(rs));
+                        //tvResult.setText(rs.toString());
+                        value1 = tvResult.getText().toString();
+                        sign = null;
+
+                    }
+                    else {
+                        int num1= Integer.parseInt(value1.toString()+"");
+                        int num2= Integer.parseInt(value2.toString()+"");
+                        double divResult=0;
+                        int rs = 0;
+
+                        if(sign=='+'){
+                            rs = num1+num2;
+                        }
+                        else if(sign=='-'){
+                            rs = num1-num2 ;
+                        }
+                        else if (sign=='*'){
+                            rs = num1*num2;
+                        }
+                        else if (sign=='/'){
+                            divResult = num1/num2;
+                        }
+                        else {
+                            if(sign=='%'){
+                                divResult=num1/100;
+                            }
+                        }
+
+                        if(sign=='/'){
+                            tvResult.setText(divResult +"");
+                            //tvResult.setText(rs.toString());
+                            value1 = tvResult.getText().toString();
+                            sign = null;
+                        }
+                        else{
+                            if(sign=='%'){
+                                tvResult.setText(divResult +"");
+                                //tvResult.setText(rs.toString());
+                                value1 = tvResult.getText().toString();
+                                sign = null;
+                                signDot=1;
+                            }
+                            else {
+                                tvResult.setText(rs +"");
+                                //tvResult.setText(rs.toString());
+                                value1 = tvResult.getText().toString();
+                                sign = null;
+                            }
+                        }
+
+
+
+                    }
+                    signEqual = 1;
                 }
 
             }
@@ -256,10 +385,11 @@ public class MainActivity extends AppCompatActivity {
         btnAC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                value1 = 0.0;
-                value2 = 0.0;
+                value1 = "";
+                value2 = "";
                 sign = null;
-                tvrs.setText("");
+                edtResult.setText("");
+                tvResult.setText("");
             }
         });
 
